7d109c97e4a94cf7b1bbe13856f07a1f0a922674
LIGA <LIGA@192.168.1.2>
2018-04-15 18:59:54 -0300

    COMPLETANDO EJERCICIO N° 1. CRISTIAN CELÍN CRUZ.

--- src/punto01/dominio/Punto.java
+++ src/punto01/dominio/Punto.java
@@ -1,5 +1,58 @@
 package punto01.dominio;
 
 public class Punto {
+    private int posicionX ;
+    private int posicionY ;
+
+    //Constructor
+    public Punto(){
     
+    }
+    
+    public Punto(int X, int Y){
+        this.posicionX = X ;
+        this.posicionY = Y ;
+    }
+    /**
+     * @return the posicionX
+     */
+    public int getPosicionX() {
+        return posicionX;
+    }
+
+    /**
+     * @param posicionX the posicionX to set
+     */
+    public void setPosicionX(int posicionX) {
+        this.posicionX = posicionX;
+    }
+
+    /**
+     * @return the posicionY
+     */
+    public int getPosicionY() {
+        return posicionY;
+    }
+
+    /**
+     * @param posicionY the posicionY to set
+     */
+    public void setPosicionY(int posicionY) {
+        this.posicionY = posicionY;
+    }
+    
+    /**
+     * Recibe un segundo punto y calcula la distancia por medio de
+     * una formula.
+     * @param punto2
+     * @return 
+     */
+    public double obtenerDistanciaEntrePuntos(Punto punto2 ){
+        //double diferenciaX = Math.pow( (punto2.getPosicionX() - this.getPosicionX()) , 2 );
+        //double diferenciaY = Math.pow( (punto2.getPosicionY() - this.getPosicionY()) , 2 );
+        //return Math.sqrt(diferenciaX + diferenciaY) ;
+
+        return Math.sqrt( Math.pow( (punto2.getPosicionX() - this.getPosicionX() ) , 2) + Math.pow( (punto2.getPosicionY() - this.getPosicionY() ) , 2)  );
+
+    }
 }
--- src/punto01/inicio/Principal.java
+++ src/punto01/inicio/Principal.java
@@ -1,5 +1,22 @@
 package punto01.inicio;
 
+import java.util.Scanner;
+import punto01.dominio.Punto;
+
 public class Principal {
-    
+     public static void main(String[] args) {
+    int x, y ;
+        Scanner teclado = new Scanner(System.in) ;
+        Punto punto1 = new Punto(7,5);
+        Punto punto2 = new Punto();
+        
+        System.out.print("Ingrese valor de X: ");
+        x = teclado.nextInt() ;
+        System.out.print("Ingrese valor de Y: ");
+        y = teclado.nextInt() ;
+        punto2.setPosicionX(x) ;
+        punto2.setPosicionY(y) ;
+        
+        System.out.println("Distancia entre el punto 1 y 2 : " + punto1.obtenerDistanciaEntrePuntos(punto2));
+}
 }
